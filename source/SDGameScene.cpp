b4b11f2b0b6c44da0b3247909c7179a5006e6829
mt-xing <mt-xing@users.noreply.github.com>
2020-02-22 16:01:48 -0500

    Format Game Scene

--- source/SDGameScene.cpp
+++ source/SDGameScene.cpp
@@ -15,11 +15,12 @@
 //  Author: Walker White
 //  Version: 1/10/17
 //
+#include "SDGameScene.h"
+
 #include <cugl/cugl.h>
+
 #include <iostream>
 #include <sstream>
-
-#include "SDGameScene.h"
 
 using namespace cugl;
 using namespace std;
@@ -28,19 +29,13 @@ using namespace std;
 #pragma mark Level Layout
 
 /** This is adjusted by screen aspect ratio to get the height */
-#define SCENE_WIDTH 1024
+constexpr unsigned int SCENE_WIDTH = 1024;
 
 /** The parallax for each layer */
-#define PARALLAX_AMT 0.1f
-
-/** Ship Frame Sprite numbers */
-#define SHIP_IMG_LEFT   0   // Left bank frame
-#define SHIP_IMG_FLAT   9   // Neutral frame
-#define SHIP_IMG_RIGHT 17   // Right bank frame
+constexpr float PARALLAX_AMT = 0.1f;
 
 #pragma mark -
 #pragma mark Constructors
-
 
 /**
  * Initializes the controller contents, and starts the game
@@ -54,56 +49,55 @@ using namespace std;
  * @return true if the controller is initialized properly, false otherwise.
  */
 bool GameScene::init(const std::shared_ptr<cugl::AssetManager>& assets) {
-    // Initialize the scene to a locked width
-    Size dimen = Application::get()->getDisplaySize();
-    dimen *= SCENE_WIDTH/dimen.width; // Lock the game to a reasonable resolution
-    if (assets == nullptr) {
-        return false;
-    } else if (!Scene::init(dimen)) {
-        return false;
-    }
-    
-    // Start up the input handler
-    _assets = assets;
-    _input.init();
-    
-    // Acquire the scene built by the asset loader and resize it the scene
-    auto scene = _assets->get<Node>("game");
-    scene->setContentSize(dimen);
-    scene->doLayout(); // Repositions the HUD
-    
-    // Get the scene components.
-    _allSpace  = _assets->get<Node>("game_field");
-    _farSpace  = _assets->get<Node>("game_field_far");
-    _nearSpace = _assets->get<Node>("game_field_near");
-    _shipNode  = std::dynamic_pointer_cast<AnimationNode>(_assets->get<Node>("game_field_player"));
-    _coordHUD  = std::dynamic_pointer_cast<Label>(_assets->get<Node>("game_hud"));
+	// Initialize the scene to a locked width
+	Size dimen = Application::get()->getDisplaySize();
+	dimen *= SCENE_WIDTH / dimen.width;	 // Lock the game to a reasonable resolution
+	if (assets == nullptr) {
+		return false;
+	} else if (!Scene::init(dimen)) {
+		return false;
+	}
 
-    // Create the ship model
-    Vec2 shipPos = _shipNode->getPosition();
-    _shipModel = ShipModel::alloc(shipPos);
-    _shipModel->setSprite(_shipNode);
+	// Start up the input handler
+	this->assets = assets;
+	input.init();
 
-    addChild(scene);
-    return true;
+	// Acquire the scene built by the asset loader and resize it the scene
+	auto scene = assets->get<Node>("game");
+	scene->setContentSize(dimen);
+	scene->doLayout();	// Repositions the HUD
+
+	// Get the scene components.
+	allSpace = assets->get<Node>("game_field");
+	farSpace = assets->get<Node>("game_field_far");
+	nearSpace = assets->get<Node>("game_field_near");
+	shipNode = std::dynamic_pointer_cast<AnimationNode>(assets->get<Node>("game_field_player"));
+	coordHUD = std::dynamic_pointer_cast<Label>(assets->get<Node>("game_hud"));
+
+	// Create the ship model
+	Vec2 shipPos = shipNode->getPosition();
+	shipModel = ShipModel::alloc(shipPos);
+	shipModel->setSprite(shipNode);
+
+	addChild(scene);
+	return true;
 }
 
 /**
  * Disposes of all (non-static) resources allocated to this mode.
  */
 void GameScene::dispose() {
-    if (_active) {
-        removeAllChildren();
-        _input.dispose();
-        _allSpace = nullptr;
-        _farSpace = nullptr;
-        _nearSpace = nullptr;
-        _shipNode = nullptr;
-        _shipModel = nullptr;
-        _active = false;
-    }
+	if (_active) {
+		removeAllChildren();
+		input.dispose();
+		allSpace = nullptr;
+		farSpace = nullptr;
+		nearSpace = nullptr;
+		shipNode = nullptr;
+		shipModel = nullptr;
+		_active = false;
+	}
 }
-
 
 #pragma mark -
 #pragma mark Gameplay Handling
@@ -112,19 +106,19 @@ void GameScene::dispose() {
  * Resets the status of the game so that we can play again.
  */
 void GameScene::reset() {
-    // Reset the ships and input
-    _shipModel->reset();
-    _input.clear();
-    
-    // Reset the parallax
-    Vec2 position = _farSpace->getPosition();
-    _farSpace->setAnchor(Vec2::ANCHOR_CENTER);
-    _farSpace->setPosition(position);
-    _farSpace->setAngle(0.0f);
-    position = _nearSpace->getPosition();
-    _nearSpace->setAnchor(Vec2::ANCHOR_CENTER);
-    _nearSpace->setPosition(position);
-    _nearSpace->setAngle(0.0f);
+	// Reset the ships and input
+	shipModel->reset();
+	input.clear();
+
+	// Reset the parallax
+	Vec2 position = farSpace->getPosition();
+	farSpace->setAnchor(Vec2::ANCHOR_CENTER);
+	farSpace->setPosition(position);
+	farSpace->setAngle(0.0f);
+	position = nearSpace->getPosition();
+	nearSpace->setAnchor(Vec2::ANCHOR_CENTER);
+	nearSpace->setPosition(position);
+	nearSpace->setAngle(0.0f);
 }
 
 /**
@@ -135,39 +129,41 @@ void GameScene::reset() {
  * @param timestep  The amount of time (in seconds) since the last frame
  */
 void GameScene::update(float timestep) {
-    _input.update(timestep);
-    
-    // Reset the game if necessary
-    if (_input.didReset()) { reset(); }
+	input.update(timestep);
 
-    Vec2 thrust = _input.getThrust();
-    
-    // Move the ship (MODEL ONLY)
-    _shipModel->setForward(thrust.y);
-    _shipModel->setTurning(thrust.x);
-    _shipModel->update(timestep);
-    
-    // "Drawing" code.  Move everything BUT the ship
-    // Update the HUD
-    _coordHUD->setText(positionText(_shipModel->getPosition()));
-    
-    Vec2 offset = _shipModel->getPosition()-_farSpace->getPosition();
-    
-    // Anchor points are in texture coordinates (0 to 1). Scale it.
-    offset.x = offset.x/_allSpace->getContentSize().width;
-    offset.y = offset.y/_allSpace->getContentSize().height;
-    
-    // Reanchor the node at the center of the screen and rotate about center.
-    Vec2 position = _farSpace->getPosition();
-    _farSpace->setAnchor(offset*PARALLAX_AMT+Vec2::ANCHOR_CENTER);
-    _farSpace->setPosition(position); // Reseting the anchor changes the position
-    _farSpace->setAngle(_shipModel->getAngle());
-    
-    // Reanchor the node at the center of the screen and rotate about center.
-    position = _nearSpace->getPosition();
-    _nearSpace->setAnchor(offset+Vec2::ANCHOR_CENTER);
-    _nearSpace->setPosition(position); // Reseting the anchor changes the position
-    _nearSpace->setAngle(_shipModel->getAngle());
+	// Reset the game if necessary
+	if (input.didReset()) {
+		reset();
+	}
+
+	Vec2 thrust = input.getThrust();
+
+	// Move the ship (MODEL ONLY)
+	shipModel->setForward(thrust.y);
+	shipModel->setTurning(thrust.x);
+	shipModel->update(timestep);
+
+	// "Drawing" code.  Move everything BUT the ship
+	// Update the HUD
+	coordHUD->setText(positionText(shipModel->getPosition()));
+
+	Vec2 offset = shipModel->getPosition() - farSpace->getPosition();
+
+	// Anchor points are in texture coordinates (0 to 1). Scale it.
+	offset.x = offset.x / allSpace->getContentSize().width;
+	offset.y = offset.y / allSpace->getContentSize().height;
+
+	// Reanchor the node at the center of the screen and rotate about center.
+	Vec2 position = farSpace->getPosition();
+	farSpace->setAnchor(offset * PARALLAX_AMT + Vec2::ANCHOR_CENTER);
+	farSpace->setPosition(position);  // Reseting the anchor changes the position
+	farSpace->setAngle(shipModel->getAngle());
+
+	// Reanchor the node at the center of the screen and rotate about center.
+	position = nearSpace->getPosition();
+	nearSpace->setAnchor(offset + Vec2::ANCHOR_CENTER);
+	nearSpace->setPosition(position);  // Reseting the anchor changes the position
+	nearSpace->setAngle(shipModel->getAngle());
 }
 
 /**
@@ -180,7 +176,8 @@ void GameScene::update(float timestep) {
  * @return an informative string for the position
  */
 std::string GameScene::positionText(const cugl::Vec2& coords) {
-    stringstream ss;
-    ss << "Coords: (" << (int)coords.x/10 << "," << (int)coords.y/10 << ")";
-    return ss.str();
+	stringstream ss;
+	constexpr unsigned int COORD_SHIFT = 10;
+	ss << "Coords: (" << (int)coords.x / COORD_SHIFT << "," << (int)coords.y / COORD_SHIFT << ")";
+	return ss.str();
 }
--- source/SDGameScene.h
+++ source/SDGameScene.h
@@ -18,10 +18,11 @@
 #ifndef __SD_GAME_SCENE_H__
 #define __SD_GAME_SCENE_H__
 #include <cugl/cugl.h>
-#include <vector>
-#include "SDShipModel.h"
-#include "SDInput.h"
 
+#include <vector>
+
+#include "SDInput.h"
+#include "SDShipModel.h"
 
 /**
  * This class is the primary gameplay constroller for the demo.
@@ -31,97 +32,94 @@
  * so that we can have a separate mode for the loading screen.
  */
 class GameScene : public cugl::Scene {
-protected:
-    /** The asset manager for this game mode. */
-    std::shared_ptr<cugl::AssetManager> _assets;
+   protected:
+	/** The asset manager for this game mode. */
+	std::shared_ptr<cugl::AssetManager> assets;
 
-    // CONTROLLERS
-    /** Controller for abstracting out input across multiple platforms */
-    ShipInput _input;
-    
-    // VIEW
-    /** Filmstrip representing the animated ship */
-    std::shared_ptr<cugl::AnimationNode> _shipNode;
-    /** Label for on-screen coordinate HUD */
-    std::shared_ptr<cugl::Label> _coordHUD;
-    /** Node to hold all of our graphics. Necesary for resolution indepedence. */
-    std::shared_ptr<cugl::Node> _allSpace;
-    /** Background in animation parallax. Stores the field of stars */
-    std::shared_ptr<cugl::Node> _farSpace;
-    /** Foreground in animation parallax. Stores the planets. */
-    std::shared_ptr<cugl::Node> _nearSpace;
+	// CONTROLLERS
+	/** Controller for abstracting out input across multiple platforms */
+	ShipInput input;
 
-    // MODEL
-    // A page-out could dispose of the view as long as it just has this.
-    /** The current coordinates of the ship */
-    std::shared_ptr<ShipModel>  _shipModel;
-    
-    /**
-     * Returns an informative string for the position
-     *
-     * This function is for writing the current ship position to the HUD.
-     *
-     * @param coords The current ship coordinates
-     *
-     * @return an informative string for the position
-     */
-    std::string positionText(const cugl::Vec2& coords);
+	// VIEW
+	/** Filmstrip representing the animated ship */
+	std::shared_ptr<cugl::AnimationNode> shipNode;
+	/** Label for on-screen coordinate HUD */
+	std::shared_ptr<cugl::Label> coordHUD;
+	/** Node to hold all of our graphics. Necesary for resolution indepedence. */
+	std::shared_ptr<cugl::Node> allSpace;
+	/** Background in animation parallax. Stores the field of stars */
+	std::shared_ptr<cugl::Node> farSpace;
+	/** Foreground in animation parallax. Stores the planets. */
+	std::shared_ptr<cugl::Node> nearSpace;
 
-    
-public:
+	// MODEL
+	// A page-out could dispose of the view as long as it just has this.
+	/** The current coordinates of the ship */
+	std::shared_ptr<ShipModel> shipModel;
+
+	/**
+	 * Returns an informative string for the position
+	 *
+	 * This function is for writing the current ship position to the HUD.
+	 *
+	 * @param coords The current ship coordinates
+	 *
+	 * @return an informative string for the position
+	 */
+	std::string positionText(const cugl::Vec2& coords);
+
+   public:
 #pragma mark -
 #pragma mark Constructors
-    /**
-     * Creates a new game mode with the default values.
-     *
-     * This constructor does not allocate any objects or start the game.
-     * This allows us to use the object without a heap pointer.
-     */
-    GameScene() : Scene() {}
-    
-    /**
-     * Disposes of all (non-static) resources allocated to this mode.
-     *
-     * This method is different from dispose() in that it ALSO shuts off any
-     * static resources, like the input controller.
-     */
-    ~GameScene() { dispose(); }
-    
-    /**
-     * Disposes of all (non-static) resources allocated to this mode.
-     */
-    void dispose();
-    
-    /**
-     * Initializes the controller contents, and starts the game
-     *
-     * The constructor does not allocate any objects or memory.  This allows
-     * us to have a non-pointer reference to this controller, reducing our
-     * memory allocation.  Instead, allocation happens in this method.
-     *
-     * @param assets    The (loaded) assets for this game mode
-     *
-     * @return true if the controller is initialized properly, false otherwise.
-     */
-    bool init(const std::shared_ptr<cugl::AssetManager>& assets);
+	/**
+	 * Creates a new game mode with the default values.
+	 *
+	 * This constructor does not allocate any objects or start the game.
+	 * This allows us to use the object without a heap pointer.
+	 */
+	GameScene() : Scene() {}
 
-    
+	/**
+	 * Disposes of all (non-static) resources allocated to this mode.
+	 *
+	 * This method is different from dispose() in that it ALSO shuts off any
+	 * static resources, like the input controller.
+	 */
+	~GameScene() { dispose(); }
+
+	/**
+	 * Disposes of all (non-static) resources allocated to this mode.
+	 */
+	void dispose() override;
+
+	/**
+	 * Initializes the controller contents, and starts the game
+	 *
+	 * The constructor does not allocate any objects or memory.  This allows
+	 * us to have a non-pointer reference to this controller, reducing our
+	 * memory allocation.  Instead, allocation happens in this method.
+	 *
+	 * @param assets    The (loaded) assets for this game mode
+	 *
+	 * @return true if the controller is initialized properly, false otherwise.
+	 */
+	bool init(const std::shared_ptr<cugl::AssetManager>& assets);
+
 #pragma mark -
 #pragma mark Gameplay Handling
-    /**
-     * The method called to update the game mode.
-     *
-     * This method contains any gameplay code that is not an OpenGL call.
-     *
-     * @param timestep  The amount of time (in seconds) since the last frame
-     */
-    void update(float timestep);
+	/**
+	 * The method called to update the game mode.
+	 *
+	 * This method contains any gameplay code that is not an OpenGL call.
+	 *
+	 * @param timestep  The amount of time (in seconds) since the last frame
+	 */
+	void update(float timestep) override;
 
-    /**
-     * Resets the status of the game so that we can play again.
-     */
-    void reset();
-
+	/**
+	 * Resets the status of the game so that we can play again.
+	 */
+	void reset() override;
 };
 
 #endif /* __SD_GAME_SCENE_H__ */
