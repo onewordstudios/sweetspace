0ba0dc4cc51b4432f60a5458d08479cf913dd63a
Sam Sorenson <sms668@cornell.edu>
2020-03-08 13:37:18 -0400

    Create DoorModel

---
+++ build-win10/Sweetspace/DoorModel.h
@@ -0,0 +1,115 @@
+﻿#ifndef __door_MODEL_H__
+#define __door_MODEL_H__
+#include <cugl/cugl.h>
+class DoorModel {
+private:
+protected:
+	/** The angle at which the door exists */
+	float angle;
+	/** The state of the door in number of players: >=2 means it is resolved */
+	int playersOn;
+	/** Reference to image in SceneGraph for animation */
+	std::shared_ptr<cugl::PolygonNode> sprite;
+
+public:
+#pragma mark Constructors
+	/*
+	 * Creates a new door at angle 0.
+	 *
+	 * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate a model on
+	 * the heap, use one of the static constructors instead.
+	 */
+	DoorModel(void) : angle(0), playersOn(0) {}
+
+	/**
+	 * Destroys this door, releasing all resources.
+	 */
+	~DoorModel(void) { dispose(); }
+
+	/**
+	 * Disposes all resources and assets of this door
+	 *
+	 * Any assets owned by this object will be immediately released.  Once
+	 * disposed, a door may not be used until it is initialized again.
+	 */
+	void dispose();
+
+	/**
+	 * Initializes a new door at an unassigned angle (-1).
+	 *
+	 * An initializer does the real work that the constructor does not.  It
+	 * initializes all assets and makes the object read for use.  By separating
+	 * them, we allow ourselfs non-pointer references to complex objects.
+	 *
+	 * @return true if the obstacle is initialized properly, false otherwise.
+	 */
+	virtual bool init() { return init(-1.0f); }
+
+	/**
+	 * Initializes a new door with the given angle
+	 *
+	 * An initializer does the real work that the constructor does not.  It
+	 * initializes all assets and makes the object read for use.  By separating
+	 * them, we allow ourselfs non-pointer references to complex objects.
+	 *
+	 * @param a   The angle at which the door exists
+	 *
+	 * @return true if the obstacle is initialized properly, false otherwise.
+	 */
+	virtual bool init(const float a) { this->angle = a; };
+
+	static std::shared_ptr<DoorModel> alloc() {
+		std::shared_ptr<DoorModel> result = std::make_shared<DoorModel>();
+		return (result->init() ? result : nullptr);
+	}
+
+#pragma mark -
+#pragma mark Accessors
+	/**
+	 * Returns the current angle of the door in radians.
+	 *
+	 * @return the current angle of the door in radians.
+	 */
+	float getAngle() { return (float)M_PI * angle / 180.0f; }
+
+	/**
+	 * Returns the number of players in range of the door.
+	 *
+	 * @return the number of players in range of the door.
+	 */
+	int getPlayersOn() { return playersOn; }
+
+	/**
+	 * Returns the current sprite of the door.
+	 *
+	 * @return the current sprite of the door.
+	 */
+	std::shared_ptr<cugl::PolygonNode> getSprite() { return sprite; }
+
+	/**
+	 * Sets the current angle of the door in radians.
+	 *
+	 * @param value The door angle in radians
+	 */
+	void setAngle(float value) { angle = 180.0f * value / (float)M_PI; }
+
+	/**
+	 * Increments the number of players in range of the door
+	 *
+	 */
+	void incPlayersOn() { playersOn++; }
+
+	/**
+	 * Decrements the number of players in range of the door
+	 *
+	 */
+	void decPlayersOn() { playersOn--; }
+
+	/**
+	 * Sets the sprite of the door.
+	 *
+	 * @param value The sprite
+	 */
+	void setSprite(const std::shared_ptr<cugl::PolygonNode> value) { sprite = value; }
+};
+#endif /* __door_MODEL_H__ */
