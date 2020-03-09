34563afdaeea7bad32f31e033188714c2b4b21e6
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-03-09 00:34:32 -0400

    Fix typos.

--- source/DonutNode.cpp
+++ source/DonutNode.cpp
@@ -1,14 +1,11 @@
-//
-// Created by Downian on 3/9/20.
-// Copyright (c) 2020 onewordstudios. All rights reserved.
-//
-
 #include "DonutNode.h"
+
 #include <cugl/2d/CUPolygonNode.h>
 
 using namespace cugl;
 
-void draw(const std::shared_ptr<SpriteBatch>& batch, const Mat4& transform, Color4 tint){
-    setPosition(donutModel.getSceneGraphPosition());
-    PolygonNode::draw(batch, transform, tint);
+void DonutNode::draw(const std::shared_ptr<cugl::SpriteBatch>& batch, const Mat4& transform,
+					 Color4 tint) {
+	setPosition(donutModel->getSceneGraphPosition());
+	PolygonNode::draw(batch, transform, tint);
 }
--- source/DonutNode.h
+++ source/DonutNode.h
@@ -1,49 +1,45 @@
-//
-// Created by Downian on 3/9/20.
-// Copyright (c) 2020 onewordstudios. All rights reserved.
-//
-
 #ifndef SWEETSPACE_DONUTNODE_H
 #define SWEETSPACE_DONUTNODE_H
 
 #include <cugl/2d/CUPolygonNode.h>
+
 #include "DonutModel.h"
 
 class DonutNode : public cugl::PolygonNode {
 #pragma mark Values
-protected:
-    std::shared_ptr<DonutModel> donutmodel;
+   protected:
+	std::shared_ptr<DonutModel> donutModel;
 
-public:
+   public:
 #pragma mark -
 #pragma mark Constructor
-    /**
-     * Creates an empty polygon with the degenerate texture.
-     *
-     * You must initialize this PolygonNode before use.
-     *
-     * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate an object on
-     * the heap, use one of the static constructors instead.
-     */
-    DonutNode() : cugl::PolygonNode() { }
+	/**
+	 * Creates an empty polygon with the degenerate texture.
+	 *
+	 * You must initialize this PolygonNode before use.
+	 *
+	 * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate an object on
+	 * the heap, use one of the static constructors instead.
+	 */
+	DonutNode() : cugl::PolygonNode() {}
 
-    /**
-     * Releases all resources allocated with this node.
-     *
-     * This will release, but not necessarily delete the associated texture.
-     * However, the polygon and drawing commands will be deleted and no
-     * longer safe to use.
-     */
-    ~DonutNode() { dispose(); }
+	/**
+	 * Releases all resources allocated with this node.
+	 *
+	 * This will release, but not necessarily delete the associated texture.
+	 * However, the polygon and drawing commands will be deleted and no
+	 * longer safe to use.
+	 */
+	~DonutNode() { dispose(); }
 
 #pragma mark -
 
-void setModel(std::shared_ptr<DonutModel> model) { donutmodel = model; }
+	void setModel(std::shared_ptr<DonutModel> model) { donutModel = model; }
 
-std::shared_ptr<DonutModel> getModel() { return donutmodel; }
+	std::shared_ptr<DonutModel> getModel() { return donutModel; }
 
-void draw(const std::shared_ptr<SpriteBatch>& cugl::batch, const cugl::Mat4& transform, cugl::Color4 tint) override;
+	void draw(const shared_ptr<cugl::SpriteBatch> &batch, const cugl::Mat4 &transform,
+			  cugl::Color4 tint) override;
 };
 
-
-#endif //SWEETSPACE_DONUTNODE_H
+#endif // SWEETSPACE_DONUTNODE_H
