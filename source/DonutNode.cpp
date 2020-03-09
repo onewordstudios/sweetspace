4d98004fa7f76b0ecdd7cd09192f888a47888184
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-03-09 00:19:28 -0400

    Create files.

---
+++ source/DonutNode.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Downian on 3/9/20.
+// Copyright (c) 2020 onewordstudios. All rights reserved.
+//
+
+#include "DonutNode.h"
---
+++ source/DonutNode.h
@@ -0,0 +1,15 @@
+//
+// Created by Downian on 3/9/20.
+// Copyright (c) 2020 onewordstudios. All rights reserved.
+//
+
+#ifndef SWEETSPACE_DONUTNODE_H
+#define SWEETSPACE_DONUTNODE_H
+
+
+class DonutNode {
+
+};
+
+
+#endif //SWEETSPACE_DONUTNODE_H
