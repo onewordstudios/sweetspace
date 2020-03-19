65a80936036afb034b59f7a907a90659ce2eafea
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-03-19 15:46:42 -0400

    Populated new node with methods

--- source/BreachNode.cpp
+++ source/BreachNode.cpp
@@ -1,6 +1,18 @@
-//
-// Created by Downian on 3/19/20.
-// Copyright (c) 2020 onewordstudios. All rights reserved.
-//
-
 #include "BreachNode.h"
+#include <cugl/2d/CUPolygonNode.h>
+
+using namespace cugl;
+
+/** The diameter of the ship. Also the x coordinate of the center of the ship */
+constexpr unsigned int DIAMETER = 1280;
+
+/** The radius of the ship. Also the y coordinate of the center of the ship */
+constexpr unsigned int RADIUS = 550;
+
+/** Pi over 180 for converting between degrees and radians */
+constexpr float PI_180 = (float)(M_PI / 180);
+
+void BreachNode::draw(const std::shared_ptr<cugl::SpriteBatch>& batch, const Mat4& transform,
+        Color4 tint) {
+    AnimationNode::draw(batch, transform, tint);
+}
--- source/BreachNode.h
+++ source/BreachNode.h
@@ -1,15 +1,60 @@
-//
-// Created by Downian on 3/19/20.
-// Copyright (c) 2020 onewordstudios. All rights reserved.
-//
-
 #ifndef SWEETSPACE_BREACHNODE_H
 #define SWEETSPACE_BREACHNODE_H
 
+#include <cugl/2d/CUAnimationNode.h>
 
-class BreachNode {
+#include "BreachModel.h"
 
+class BreachNode : public cugl::AnimationNode {
+#pragma mark Values
+protected:
+    std::shared_ptr<BreachModel> breachModel;
+
+public:
+#pragma mark -
+#pragma mark Constructor
+    /**
+     * Creates an empty Breach with the degenerate texture.
+     *
+     * You must initialize this BreachNode before use.
+     *
+     * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate an object on
+     * the heap, use one of the static constructors instead.
+     */
+    BreachNode() : cugl::AnimationNode() {}
+
+    /**
+     * Releases all resources allocated with this node.
+     *
+     * This will release, but not necessarily delete the associated texture.
+     * However, the breach and drawing commands will be deleted and no
+     * longer safe to use.
+     */
+    ~BreachNode() { dispose(); }
+
+#pragma mark -
+    /**
+     * Returns a textured breach from a Texture object.
+     *
+     * After creation, the breach will be a rectangle. The vertices of this
+     * breach will be the corners of the texture.
+     *
+     * @param texture   A shared pointer to a Texture object.
+     *
+     * @return a textured breach from a Texture object.
+     */
+    static std::shared_ptr<BreachNode> allocWithTexture(
+            const std::shared_ptr<cugl::Texture> &texture) {
+        std::shared_ptr<BreachNode> node = std::make_shared<BreachNode>();
+        return (node->initWithTexture(texture) ? node : nullptr);
+    }
+
+    void setModel(std::shared_ptr<BreachModel> model) { breachModel = model; }
+
+    std::shared_ptr<BreachModel> getModel() { return breachModel; }
+
+    void draw(const shared_ptr<cugl::SpriteBatch> &batch, const cugl::Mat4 &transform,
+            cugl::Color4 tint) override;
 };
-
 
 #endif //SWEETSPACE_BREACHNODE_H
