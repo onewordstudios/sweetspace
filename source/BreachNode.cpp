b5156fe737251f3afd1ba60233537470ae9de53f
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-03-19 14:50:01 -0400

    Add new node files

---
+++ source/BreachNode.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Downian on 3/19/20.
+// Copyright (c) 2020 onewordstudios. All rights reserved.
+//
+
+#include "BreachNode.h"
---
+++ source/BreachNode.h
@@ -0,0 +1,15 @@
+//
+// Created by Downian on 3/19/20.
+// Copyright (c) 2020 onewordstudios. All rights reserved.
+//
+
+#ifndef SWEETSPACE_BREACHNODE_H
+#define SWEETSPACE_BREACHNODE_H
+
+
+class BreachNode {
+
+};
+
+
+#endif //SWEETSPACE_BREACHNODE_H
