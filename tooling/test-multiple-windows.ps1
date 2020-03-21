5a1be1df4c0534c3bc477012582550af7a864303
mt-xing <mt-xing@users.noreply.github.com>
2020-03-20 20:53:16 -0400

    PS Script Instructions

--- tooling/test-multiple-windows.ps1
+++ tooling/test-multiple-windows.ps1
@@ -1,4 +1,18 @@
-﻿# Clean output folder (delete if exists)
+﻿# ================
+#   INSTRUCTIONS
+# ================
+#
+# FIRST-TIME SETUP:
+# First, open an administrator PowerShell window
+# (right click the start button and choose "Windows PowerShell (Admin)"
+# Run `set-executionpolicy remotesigned` (without quotes) to enable scripts
+# If it runs now, you're done. Otherwise, you may need to copy the contents of this file,
+# delete it, recreate it, and paste the contents back in
+#
+# NORMAL USE:
+# Right click this file and choose "Run with PowerShell"
+
+# Clean output folder (delete if exists)
 Remove-Item windows-build -Recurse -ErrorAction Ignore
 
 # Create fresh output folder
