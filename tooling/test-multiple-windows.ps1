f672b015b17e6e9252f06df77a505b9a4a3476bc
mt-xing <mt-xing@users.noreply.github.com>
2020-03-20 20:27:36 -0400

    Windows Testing Script

---
+++ tooling/test-multiple-windows.ps1
@@ -0,0 +1,14 @@
+﻿# Clean output folder (delete if exists)
+Remove-Item windows-build -Recurse -ErrorAction Ignore
+
+# Create fresh output folder
+New-Item -ItemType directory -Path windows-build
+
+# Copy DLLs
+Copy-Item -Path ..\build-win10\dll\x64\* -Destination windows-build
+
+# Copy Assets
+Copy-Item -Path ..\assets\* -Destination windows-build -Recurse
+
+# Copy EXE
+Copy-Item -Path ..\build-win10\x64\Debug\Sweetspace.exe -Destination windows-build
