46a6616143cf5cf0e73fe023c437a2d99eef569a
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-03-23 15:34:24 -0400

    Refactoring wip

--- source/BreachNode.cpp
+++ source/BreachNode.cpp
@@ -1,4 +1,5 @@
 #include "BreachNode.h"
+#include "GameGraphRoot.h"
 #include <cugl/2d/CUPolygonNode.h>
 
 using namespace cugl;
@@ -14,5 +15,17 @@ constexpr float PI_180 = (float)(M_PI / 180);
 
 void BreachNode::draw(const std::shared_ptr<cugl::SpriteBatch>& batch, const Mat4& transform,
         Color4 tint) {
+    if (breachModel->getHealth() > 0) {
+        Vec2 breachPos = Vec2(DIAMETER + RADIUS * sin(breachModel->getAngle()),
+                DIAMETER / 2.0f - RADIUS * cos(breachModel->getAngle()));
+        if (breachModel->getAngle() < 0) {
+            breachPos = Vec2(0, 0);
+        }
+        setScale(GameGraphRoot::BREACH_SCALE * breachModel->getHealth() / 3.0f);
+        setPosition(breachPos);
+    } else {
+        Vec2 breachPos = Vec2(0, 0);
+        setPosition(breachPos);
+    }
     AnimationNode::draw(batch, transform, tint);
 }
--- source/BreachNode.h
+++ source/BreachNode.h
@@ -7,54 +7,64 @@
 
 class BreachNode : public cugl::AnimationNode {
 #pragma mark Values
-protected:
-    std::shared_ptr<BreachModel> breachModel;
+   protected:
+	/** Reference to the model of this node. */
+	std::shared_ptr<BreachModel> breachModel;
 
-public:
+   public:
 #pragma mark -
+	static constexpr int BREACH_H = 1;
+	static constexpr int BREACH_W = 1;
 #pragma mark Constructor
-    /**
-     * Creates an empty Breach with the degenerate texture.
-     *
-     * You must initialize this BreachNode before use.
-     *
-     * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate an object on
-     * the heap, use one of the static constructors instead.
-     */
-    BreachNode() : cugl::AnimationNode() {}
+	/**
+	 * Creates an empty Breach with the degenerate texture.
+	 *
+	 * You must initialize this BreachNode before use.
+	 *
+	 * NEVER USE A CONSTRUCTOR WITH NEW. If you want to allocate an object on
+	 * the heap, use one of the static constructors instead.
+	 */
+	BreachNode() : cugl::AnimationNode() {}
 
-    /**
-     * Releases all resources allocated with this node.
-     *
-     * This will release, but not necessarily delete the associated texture.
-     * However, the breach and drawing commands will be deleted and no
-     * longer safe to use.
-     */
-    ~BreachNode() { dispose(); }
+	/**
+	 * Releases all resources allocated with this node.
+	 *
+	 * This will release, but not necessarily delete the associated texture.
+	 * However, the breach and drawing commands will be deleted and no
+	 * longer safe to use.
+	 */
+	~BreachNode() { dispose(); }
+
+	/**
+	 * Returns a textured breach from a Texture object.
+	 *
+	 * After creation, the breach will be a rectangle. The vertices of this
+	 * breach will be the corners of the texture.
+	 *
+	 * @param texture   A shared pointer to a Texture object.
+	 *
+	 * @return a textured breach from a Texture object.
+	 */
+	static std::shared_ptr<BreachNode> allocWithTexture(
+		const std::shared_ptr<cugl::Texture> &texture) {
+		std::shared_ptr<BreachNode> node = std::make_shared<BreachNode>();
+		return (node->initWithFilmstrip(texture, BREACH_H, BREACH_W) ? node : nullptr);
+	}
 
 #pragma mark -
-    /**
-     * Returns a textured breach from a Texture object.
-     *
-     * After creation, the breach will be a rectangle. The vertices of this
-     * breach will be the corners of the texture.
-     *
-     * @param texture   A shared pointer to a Texture object.
-     *
-     * @return a textured breach from a Texture object.
-     */
-    static std::shared_ptr<BreachNode> allocWithTexture(
-            const std::shared_ptr<cugl::Texture> &texture) {
-        std::shared_ptr<BreachNode> node = std::make_shared<BreachNode>();
-        return (node->initWithTexture(texture) ? node : nullptr);
-    }
+#pragma mark Getters & Setters
 
-    void setModel(std::shared_ptr<BreachModel> model) { breachModel = model; }
+	void setModel(std::shared_ptr<BreachModel> model) {
+		breachModel = model;
+	}
 
-    std::shared_ptr<BreachModel> getModel() { return breachModel; }
+	std::shared_ptr<BreachModel> getModel() { return breachModel; }
 
-    void draw(const shared_ptr<cugl::SpriteBatch> &batch, const cugl::Mat4 &transform,
-            cugl::Color4 tint) override;
+#pragma mark -
+#pragma mark Drawing
+
+	void draw(const shared_ptr<cugl::SpriteBatch> &batch, const cugl::Mat4 &transform,
+			  cugl::Color4 tint) override;
 };
 
-#endif //SWEETSPACE_BREACHNODE_H
+#endif // SWEETSPACE_BREACHNODE_H
