a9c99e9d75d8d6ec2d730888619bbdd89c883655
Aashna Saxena <as2996@cornell.edu>
2020-03-28 22:26:51 -0700

    clang format

--- source/HealthNode.cpp
+++ source/HealthNode.cpp
@@ -24,36 +24,36 @@ const Vec2 pos5 = Vec2(-145 + RADIUS * sin(0), (RADIUS - 410) * cos(0));
 
 const Vec2 pos6 = Vec2(-202 + RADIUS * sin(0), cos(0));
 
-const Vec2 pos7 = Vec2(- 147 + RADIUS * sin(0), -142 * cos(0));
+const Vec2 pos7 = Vec2(-147 + RADIUS * sin(0), -142 * cos(0));
 
 void HealthNode::draw(const std::shared_ptr<cugl::SpriteBatch>& batch, const Mat4& transform,
 					  Color4 tint) {
-    switch(section) {
-        case 0:
-            setPosition(pos0);
-            break;
-        case 1:
-            setPosition(pos1);
-            break;
-        case 2:
-            setPosition(pos2);
-            break;
-        case 3:
-            setPosition(pos3);
-            break;
-        case 4:
-            setPosition(pos4);
-            break;
-        case 5:
-            setPosition(pos5);
-            break;
-        case 6:
-            setPosition(pos6);
-            break;
-        case 7:
-            setPosition(pos7);
-            break;
-    }
+	switch (section) {
+		case 0:
+			setPosition(pos0);
+			break;
+		case 1:
+			setPosition(pos1);
+			break;
+		case 2:
+			setPosition(pos2);
+			break;
+		case 3:
+			setPosition(pos3);
+			break;
+		case 4:
+			setPosition(pos4);
+			break;
+		case 5:
+			setPosition(pos5);
+			break;
+		case 6:
+			setPosition(pos6);
+			break;
+		case 7:
+			setPosition(pos7);
+			break;
+	}
 
 	setAngle((float)(M_PI * 45 * section) / 180.0f);
 	ship->getHealth() > 11 ? setFrame(11) : setFrame(ship->getHealth());
