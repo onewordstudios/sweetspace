7e6ae7e52ff23b58c19a86eb94933888b0a767cc
Sam Sorenson <sms668@cornell.edu>
2020-04-16 18:44:00 -0400

    remove set health

--- source/ShipModel.cpp
+++ source/ShipModel.cpp
@@ -83,7 +83,6 @@ bool ShipModel::createAllTask(int data) {
 }
 
 bool ShipModel::failAllTask() {
-	setHealth(health - 1);
 	for (int i = 0; i < donuts.size(); i++) {
 		float angle = (float)(rand() % (int)(getSize()));
 		donuts.at(i)->setAngle(angle);
