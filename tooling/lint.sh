389a0c00de59c5589f6f4c1e2dfee549917c5c41
mt-xing <mt-xing@users.noreply.github.com>
2020-04-19 17:23:52 -0400

    Update Lint Script for CI

--- tooling/lint.sh
+++ tooling/lint.sh
@@ -5,11 +5,15 @@ then
     echo ''
     echo '    To run the linter on a single file, pass in the name of the cpp file as the argument to this script (without the .cpp)'
     echo '    For example, run:         ./lint.sh GLaDOS'
-    echo '    Pass the word "all" as an argument to run on all source files (will take a while)'
+    echo '    Pass the flag "-all" as an argument to run on all source files (will take a while)'
+	echo '    The flag "-ci" is used by the CI pipeline; please ignore it'
     echo ''
-elif [ $1 == 'all' ]
+elif [ $1 == '-all' ]
 then
     clang-tidy ../source/*.cpp -- -I../cugl/include
+elif [ $1 == '-ci' ]
+then
+	clang-tidy ../source/*.cpp -- -I../cugl/include | head -n -2 | grep -v "warnings generated."
 else
     clang-tidy ../source/$1.cpp -- -I../cugl/include
 fi
