a75e0667c78452f0c9b0ef2bc6e45afe2d0205b3
mt-xing <mt-xing@users.noreply.github.com>
2020-04-19 17:41:05 -0400

    Fix Spacing

--- tooling/lint.sh
+++ tooling/lint.sh
@@ -6,7 +6,7 @@ then
     echo '    To run the linter on a single file, pass in the name of the cpp file as the argument to this script (without the .cpp)'
     echo '    For example, run:         ./lint.sh GLaDOS'
     echo '    Pass the flag "-all" as an argument to run on all source files (will take a while)'
-	echo '    The flag "-ci" is used by the CI pipeline; please ignore it'
+    echo '    The flag "-ci" is used by the CI pipeline; please ignore it'
     echo ''
 elif [ $1 == '-all' ]
 then
