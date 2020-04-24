65720725db08b45de11a9a9fe1ca95d603daae98
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-04-24 15:24:36 -0400

    Added real jumping animation

--- source/DonutNode.cpp
+++ source/DonutNode.cpp
@@ -9,12 +9,42 @@
 using namespace cugl;
 
 /** The scale by which donut stretches when jumping */
-constexpr float JUMP_SCALE = 1.6f;
+constexpr float JUMP_SCALE = 0.6f;
+
+/** Percentage of jump at which distortion begins */
+constexpr float SCALING_BEGIN = 0.1f;
+
+/** Percentage of jump at which distortion stops */
+constexpr float SCALING_END = 1.2f;
 
 void DonutNode::animateJumping() {
-	setScale(GameGraphRoot::DONUT_SCALE,
-			 Tween::linear(GameGraphRoot::DONUT_SCALE, GameGraphRoot::DONUT_SCALE * JUMP_SCALE,
-						   (int)(donutModel->getJumpOffset() * 100), 35));
+	float halfJumpTime =
+		sqrt(2 * DonutModel::GRAVITY * DonutModel::JUMP_HEIGHT) / DonutModel::GRAVITY;
+	float scalingWindowSize = halfJumpTime * (SCALING_END - SCALING_BEGIN);
+	bool isInScalingWindow = donutModel->getJumpTime() > halfJumpTime * SCALING_BEGIN &&
+							 donutModel->getJumpTime() < halfJumpTime * SCALING_END;
+	if (!donutModel->isJumping()) {
+		// Not jumping. Set scale to normal and return
+		setScale(GameGraphRoot::DONUT_SCALE, GameGraphRoot::DONUT_SCALE);
+		return;
+	}
+	float xScale;
+	if (donutModel->getJumpTime() <= halfJumpTime * SCALING_BEGIN) {
+		// First animation stage
+		xScale = Tween::linear(GameGraphRoot::DONUT_SCALE, GameGraphRoot::DONUT_SCALE * JUMP_SCALE,
+							   (int)((donutModel->getJumpTime()) * 100),
+							   (int)(halfJumpTime * SCALING_BEGIN * 100));
+	} else if (isInScalingWindow) {
+		// Second animation stage
+		xScale =
+			Tween::linear(GameGraphRoot::DONUT_SCALE * JUMP_SCALE, GameGraphRoot::DONUT_SCALE,
+						  (int)((donutModel->getJumpTime() - halfJumpTime * SCALING_BEGIN) * 100),
+						  (int)(scalingWindowSize * 100));
+	} else {
+		// Not in animation stage
+		xScale = GameGraphRoot::DONUT_SCALE;
+	}
+	setScale(xScale, GameGraphRoot::DONUT_SCALE);
 }
 
 void DonutNode::animateFacialExpression() {}
