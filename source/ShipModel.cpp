1e1c7cf42962609c41070efa458dd464c43bec66
Aashna Saxena <as2996@cornell.edu>
2020-04-26 16:09:18 -0700

    fix all player challenge

--- source/ShipModel.cpp
+++ source/ShipModel.cpp
@@ -86,6 +86,24 @@ bool ShipModel::createAllTask(int data) {
 bool ShipModel::failAllTask() {
 	for (int i = 0; i < donuts.size(); i++) {
 		float angle = (float)(rand() % (int)(getSize()));
+		bool goodAngle = false;
+		while(!goodAngle) {
+			angle = (float)(rand() % (int)(getSize()));
+			for (unsigned int k = 0; k < breaches.size(); k++) {
+				float breachAngle = breaches[k]->getAngle();
+				float diff = shipSize / 2 - abs(abs(breachAngle - angle) - shipSize / 2);
+				if (breachAngle == -1 || diff > 15) {
+					goodAngle = true;
+				}
+			}
+			for (unsigned int k = 0; k < doors.size(); k++) {
+				float doorAngle = doors[k]->getAngle();
+				float diff = shipSize / 2 - abs(abs(doorAngle - angle) - shipSize / 2);
+				if (doorAngle == -1 || diff > 15) {
+					goodAngle = true;
+				}
+			}
+		}
 		donuts.at(i)->setAngle(angle);
 	}
 	return true;
