fa1a9d0073c0d86d58bbbb60dff26d87c7b58385
Wenjia Zhang <wenjia.zhang@sibelhealth.com>
2020-04-29 18:13:40 -0400

    Added CustomNode interface class

---
+++ source/CustomNode.h
@@ -0,0 +1,71 @@
+#ifndef SWEETSPACE_CUSTOMNODE_H
+#define SWEETSPACE_CUSTOMNODE_H
+
+#include <cugl/2d/CUNode.h>
+
+#include "DonutModel.h"
+#include "Globals.h"
+
+/*
+ * This class acts as an interface for game objects that exist in the level and require
+ * view boundary calculations.
+ * Note: if the node is not a child of nearSpace, it probably should not inherit this class.
+ */
+class CustomNode : public cugl::Node {
+#pragma mark Fields
+   protected:
+	/** Reference to the player donut model */
+	std::shared_ptr<DonutModel> playerDonutModel;
+	/** Size of the ship. Needed for visibility determination */
+	float shipSize;
+	/** Whether the breach is being shown right now */
+	bool isShown;
+#pragma mark -
+#pragma mark Setters
+	void setDonutModel(std::shared_ptr<DonutModel> model) { playerDonutModel = model; }
+
+	void setShipSize(float f) { shipSize = f; }
+#pragma mark -
+#pragma mark Positioning Methods
+	/**
+	 * Calculates the on-screen angle of node relative to the player avatar
+	 *
+	 * @param modelAngle Angle of this node's model
+	 * @return
+	 */
+	float getOnScreenAngle(float modelAngle) {
+		float onScreenAngle = modelAngle - playerDonutModel->getAngle();
+		onScreenAngle = onScreenAngle >= 0 ? onScreenAngle : shipSize + onScreenAngle;
+		onScreenAngle = onScreenAngle > shipSize / 2 ? onScreenAngle - shipSize : onScreenAngle;
+		onScreenAngle *= globals::PI_180;
+		return onScreenAngle;
+	}
+
+	/**
+	 * Returns true if this node is just coming into viewing bounds
+	 *
+	 * @param onScreenAngle The on-screen angle of node relative to the player avatar
+	 * @return
+	 */
+	bool isComingIntoView(float onScreenAngle) {
+		return !isShown && onScreenAngle < globals::SEG_CUTOFF_ANGLE &&
+			   onScreenAngle > -globals::SEG_CUTOFF_ANGLE;
+	}
+
+	/**
+	 * Returns true if this node is just going out of viewing bounds
+	 *
+	 * @param onScreenAngle The on-screen angle of node relative to the player avatar
+	 * @return
+	 */
+	bool isGoingOutOfView(float onScreenAngle) {
+		return isShown && (onScreenAngle >= globals::SEG_CUTOFF_ANGLE ||
+						   onScreenAngle <= -globals::SEG_CUTOFF_ANGLE);
+	}
+
+	Vec2 getPositionVec(float relAngle, float radius) {
+		return Vec2(radius * sin(relAngle), -radius * cos(relAngle));
+	}
+};
+
+#endif // SWEETSPACE_CUSTOMNODE_H
