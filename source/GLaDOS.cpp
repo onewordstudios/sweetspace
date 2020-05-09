5c082f35b4f463e7af01dedd6876a2363c26038a
Sam Sorenson <sms668@cornell.edu>
2020-05-09 19:44:02 -0400

    don't do tutorial stuff in easy level

--- source/GLaDOS.cpp
+++ source/GLaDOS.cpp
@@ -43,6 +43,7 @@ void GLaDOS::dispose() {
  * @return true if the controller was initialized successfully
  */
 bool GLaDOS::init(std::shared_ptr<ShipModel> ship, std::shared_ptr<LevelModel> level) {
+	levelNum = mib->getLevelNum();
 	bool success = true;
 	this->ship = ship;
 	this->mib = MagicInternetBox::getInstance();
