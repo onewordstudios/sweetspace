52a9e0206c5a750e1e95cf6aa631a4d74ea4cc4b
mt-xing <mt-xing@users.noreply.github.com>
2020-06-03 21:30:26 -0400

    Fix CUGL High DPI for Windows

--- cugl/lib/base/CUDisplay.cpp
+++ cugl/lib/base/CUDisplay.cpp
@@ -114,6 +114,41 @@ bool Display::init(std::string title, Rect bounds, Uint32 flags) {
     Uint32 sdlflags = SDL_WINDOW_HIDDEN | SDL_WINDOW_OPENGL;
     if (flags & INIT_HIGH_DPI) {
         sdlflags |= SDL_WINDOW_ALLOW_HIGHDPI;
+#if CU_PLATFORM == CU_PLATFORM_WINDOWS
+		typedef enum PROCESS_DPI_AWARENESS {
+			PROCESS_DPI_UNAWARE = 0,
+			PROCESS_SYSTEM_DPI_AWARE = 1,
+			PROCESS_PER_MONITOR_DPI_AWARE = 2
+		} PROCESS_DPI_AWARENESS;
+
+		void* userDLL;
+		BOOL(WINAPI * SetProcessDPIAware)(void); // Vista and later
+		void* shcoreDLL;
+		HRESULT(WINAPI * SetProcessDpiAwareness)(PROCESS_DPI_AWARENESS dpiAwareness); // Windows 8.1 and later
+
+		userDLL = SDL_LoadObject("USER32.DLL");
+		if (userDLL) {
+			SetProcessDPIAware = (BOOL(WINAPI*)(void)) SDL_LoadFunction(userDLL, "SetProcessDPIAware");
+		}
+
+		shcoreDLL = SDL_LoadObject("SHCORE.DLL");
+		if (shcoreDLL) {
+			SetProcessDpiAwareness = (HRESULT(WINAPI*)(PROCESS_DPI_AWARENESS)) SDL_LoadFunction(shcoreDLL, "SetProcessDpiAwareness");
+		}
+
+		if (SetProcessDpiAwareness) {
+			/* Try Windows 8.1+ version */
+			HRESULT result = SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+			SDL_Log("called SetProcessDpiAwareness: %d", (result == S_OK) ? 1 : 0);
+		}
+		else if (SetProcessDPIAware) {
+			/* Try Vista - Windows 8 version.
+			This has a constant scale factor for all monitors.
+			*/
+			BOOL success = SetProcessDPIAware();
+			SDL_Log("called SetProcessDPIAware: %d", (int)success);
+		}
+#endif
     }
 
     _bounds  = DisplayBounds();
