3681953dfb451e08eaff5441bb866e188503a7b8
mt-xing <mt-xing@users.noreply.github.com>
2021-01-15 20:41:57 -0500

    Fix More Clang Stuff

--- source/WinScreen.cpp
+++ source/WinScreen.cpp
@@ -72,12 +72,13 @@ void WinScreen::dispose() {
 }
 
 float levelToPos(uint8_t lvl) {
-	auto x = std::adjacent_find(LEVEL_ENTRY_POINTS.begin(), LEVEL_ENTRY_POINTS.end(),
+	const auto *x = std::adjacent_find(LEVEL_ENTRY_POINTS.begin(), LEVEL_ENTRY_POINTS.end(),
 								[=](uint8_t a, uint8_t b) { return a <= lvl && b > lvl; });
 	if (x == LEVEL_ENTRY_POINTS.end()) {
 		// Past the end
 
-		uint8_t lastEntry = *(--LEVEL_ENTRY_POINTS.end());
+		const auto *endIt = LEVEL_ENTRY_POINTS.end();
+		uint8_t lastEntry = *(--endIt);
 		float t =
 			static_cast<float>(lvl - lastEntry) / static_cast<float>(MAX_NUM_LEVELS - lastEntry);
 		float slice = 1.f / (LEVEL_ENTRY_POINTS.size() - 1);
