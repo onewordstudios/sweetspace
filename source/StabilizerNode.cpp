34f65baa252e09b3abbc98181616c4572ff55aa2
mt-xing <mt-xing@users.noreply.github.com>
2021-01-23 22:48:02 -0500

    Oops

--- source/StabilizerNode.cpp
+++ source/StabilizerNode.cpp
@@ -130,6 +130,7 @@ void StabilizerNode::update() {
 				failPanel->setVisible(true);
 				failPanel->setAnchor(1.f / 2, 0);
 				doLayout();
+				currFrame = 0;
 			}
 			break;
 		}
@@ -163,7 +164,6 @@ void StabilizerNode::update() {
 			failPanel->setAnchor({1.f / 2, Tween::easeIn(1, 0, currFrame, ANIMATE_TIME)});
 			doLayout();
 			if (currFrame == ANIMATE_TIME) {
-				currFrame = 0;
 				state = NodeStatus::Off;
 				setVisible(false);
 			}
