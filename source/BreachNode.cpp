9aaebb8ec9f2be132c5e49fbc80c26c66caad9d3
mt-xing <mt-xing@users.noreply.github.com>
2021-06-03 12:37:29 -0400

    Naming

--- source/BreachNode.cpp
+++ source/BreachNode.cpp
@@ -116,14 +116,15 @@ void BreachNode::postPosition() {
 		}
 	} else {
 		// Play idle animation
-		int magicNum =
+		unsigned int twiceStripLength = 2 * NUM_IDLE_FRAMES * NUM_SKIP_FRAMES;
+		int frameNum =
 			(currentFrameIdle < NUM_IDLE_FRAMES * NUM_SKIP_FRAMES
 				 ? currentFrameIdle / NUM_SKIP_FRAMES
-				 : (NUM_IDLE_FRAMES * NUM_SKIP_FRAMES * 2 - currentFrameIdle) / NUM_SKIP_FRAMES);
-		shapeNode->setFrame(static_cast<int>(getFrameFromHealth(breachModel->getHealth())) +
-							magicNum);
+							: (twiceStripLength - currentFrameIdle) / NUM_SKIP_FRAMES);
+		unsigned int frameOffset = getFrameFromHealth(breachModel->getHealth());
+		shapeNode->setFrame(static_cast<int>(frameOffset) + frameNum);
 		patternNode->setFrame(static_cast<int>(shapeNode->getFrame()));
-		currentFrameIdle = currentFrameIdle == NUM_IDLE_FRAMES * 2 * NUM_SKIP_FRAMES - 1
+		currentFrameIdle = currentFrameIdle == twiceStripLength - 1
 							   ? 0
 							   : currentFrameIdle + 1;
 	}
