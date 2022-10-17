0ddcd9c448dc169313533803947e0e08e9ebd8f2
mt-xing <mt-xing@users.noreply.github.com>
2022-10-16 19:23:56 -0700

    iOS release

--- source/AdUtils.cpp
+++ source/AdUtils.cpp
@@ -7,8 +7,8 @@ const char* const K_BANNER_AD_UNIT = "ca-app-pub-3940256099942544/6300978111";
 const char* const K_INTERSTITIAL_AD_UNIT = "ca-app-pub-3940256099942544/1033173712";
 #else
 // iOS ad unit IDs
-const char* const K_BANNER_AD_UNIT = "ca-app-pub-3940256099942544/2934735716";
-const char* const K_INTERSTITIAL_AD_UNIT = "ca-app-pub-3940256099942544/4411468910";
+const char* const K_BANNER_AD_UNIT = "ca-app-pub-9909379902934039/5955918815";
+const char* const K_INTERSTITIAL_AD_UNIT = "ca-app-pub-9909379902934039/2995531792";
 #endif
 
 #if defined(__ANDROID__) || defined(__IPHONEOS__)
